1676dcfc9df5fe6814412bb0ac53b4dfd8c672f0
Avinash Rao <avinashrao2002@gmail.com>
2021-02-21 21:07:53 -0800

    created messages table and used the Sequelize orm to return data with graphql

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,23 @@ const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const fs = require("fs");
 const path = require("path");
-
+const connectToDb = require("./connection");
+connectToDb();
+const Message = require("./Message");
 const resolvers = {
   Query: {
     info: () => "Hello World",
+  },
+  Mutation: {
+    createMessage: async (parent, { body }) => {
+      await Message.sync({ force: true }); //.sync creates
+      // a table and force:
+      // true removes all the previous messages
+      // and creates a new one
+      let m = Message.build({ body });
+      await m.save();
+      return m;
+    },
   },
 };
 
